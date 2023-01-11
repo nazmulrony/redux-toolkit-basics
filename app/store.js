b234c5e2239b2dc93bfa68cc614ffea6b11e4d61
Shahadat_Robin <robin@corexlab.dev>
2023-01-11 15:07:25 +0600

    most important redux thunk used in redux toolkit

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceCreamReducer = require("../features/icecream/iceCreamSlice");
+const userReducer = require("../features/user/userSlice");
 
 //logger middleware
 const reduxLogger = require("redux-logger");
@@ -11,6 +12,7 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         iceCream: iceCreamReducer,
+        user: userReducer,
     },
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require("./app/store");
 const { iceCreamActions } = require("./features/icecream/iceCreamSlice");
+const { fetchUsers } = require("./features/user/userSlice");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 // const iceCreamActions =
 
@@ -8,12 +9,14 @@ const unsubscribe = store.subscribe(() => {
     console.log("Updated State", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.reStocked(3));
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.reStocked(6));
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.reStocked(3));
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.reStocked(6));
+
+store.dispatch(fetchUsers());
+// unsubscribe();
